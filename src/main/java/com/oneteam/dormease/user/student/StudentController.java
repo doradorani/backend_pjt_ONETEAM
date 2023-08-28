3d3b91097567d559d2ad4575dbbab01693a2b324
doradorani <sokoyo2@gmail.com>
2023-08-28 16:21:51 +0900

    git commit -m "OB-20 회원가입 폼"

--- src/main/java/com/oneteam/dormease/user/student/StudentController.java
+++ src/main/java/com/oneteam/dormease/user/student/StudentController.java
@@ -2,7 +2,33 @@ package com.oneteam.dormease.user.student;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @Log4j2
 @Controller
+@RequestMapping("/user/student")
 public class StudentController {
+
+    @GetMapping("/createAccountForm")
+    public String createAccountForm() {
+        log.info("createAccountForm()");
+
+        String nextPage = "/";
+
+        return nextPage;
+
+    }
+
+    @PostMapping("/createAccountConfirm")
+    public String createAccountConfirm(StudentDto studentDto) {
+        log.info("createAccountConfirm()");
+
+        String nextPage = "";
+
+        return nextPage;
+
+    }
 }
