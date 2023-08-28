9a2f766c3efd108e799c52ba967ffffdb85fa46a
ahnjisoo <wltn24996@gmail.com>
2023-08-28 11:31:19 +0900

    init new project

---
+++ src/main/java/com/oneteam/dormease/DormEaseApplication.java
@@ -0,0 +1,13 @@
+package com.oneteam.dormease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormEaseApplication.class, args);
+	}
+
+}
