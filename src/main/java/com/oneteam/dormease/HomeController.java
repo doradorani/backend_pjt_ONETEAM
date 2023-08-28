32e8afc5267366b737fe14e88ecf02386b2ba844
doradorani <sokoyo2@gmail.com>
2023-08-28 14:22:59 +0900

    OB-18 application.properties 추가

--- src/main/java/com/oneteam/dormease/HomeController.java
+++ src/main/java/com/oneteam/dormease/HomeController.java
@@ -2,7 +2,16 @@ package com.oneteam.dormease;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @Log4j2
-@Controller
+@RestController
 public class HomeController {
+    @RequestMapping(value = {"", "/"})
+    public String home() {
+        log.info("home");
+
+        return "home";
+    }
 }
