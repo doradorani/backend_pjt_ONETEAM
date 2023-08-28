c70eefeda9ac14210ec266948545af23bd528870
ahnjisoo <wltn24996@gmail.com>
2023-08-28 13:57:12 +0900

    UserDto 초안 작성 (수정 필요)

--- src/main/java/com/oneteam/dormease/user/parents/ParentsDto.java
+++ src/main/java/com/oneteam/dormease/user/parents/ParentsDto.java
@@ -1,4 +1,12 @@
 package com.oneteam.dormease.user.parents;
 
+import lombok.Data;
+@Data
 public class ParentsDto {
+    private int no;
+    private String id;
+    private String pw;
+    private String name;
+    private String phone;
+    private String mail;
 }
--- src/main/java/com/oneteam/dormease/user/student/StudentDto.java
+++ src/main/java/com/oneteam/dormease/user/student/StudentDto.java
@@ -1,4 +1,18 @@
 package com.oneteam.dormease.user.student;
 
+import lombok.Data;
+
+@Data
 public class StudentDto {
+    private int no;
+    private String id;
+    private String pw;
+    private String name;
+    private String schName;
+    private String grade;
+    private String classNo;
+    private String birth;
+    private String addr;
+    private String phone;
+    private String mail;
 }
