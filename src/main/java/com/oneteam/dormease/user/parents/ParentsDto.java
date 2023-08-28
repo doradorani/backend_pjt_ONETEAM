5451b491cae946f3a7d168245f85a07c020dd247
ahnjisoo <wltn24996@gmail.com>
2023-08-28 18:25:12 +0900

    git commit -m "OB-21 사용자 페이지 DTO생성 및 코드 컨벤션 적용"

--- src/main/java/com/oneteam/dormease/user/parents/ParentsDto.java
+++ src/main/java/com/oneteam/dormease/user/parents/ParentsDto.java
@@ -3,10 +3,12 @@ package com.oneteam.dormease.user.parents;
 import lombok.Data;
 @Data
 public class ParentsDto {
-    private int no;
-    private String id;
-    private String pw;
-    private String name;
-    private String phone;
-    private String mail;
+    private int no;                 // COMMENT '학부모 번호'
+    private int student_no;         // COMMENT '학생 번호'
+    private int relation;           // COMMENT '학부모 관계'
+    private String phone;           // COMMENT '학부모 전화 번호'
+    private String mail;            // COMMENT '학부모 메일'
+    private String address;         // COMMENT '학부모 주소'
+    private String reg_date;        // COMMENT '등록 일자'
+    private String mod_date;        // COMMENT '수정 일자'
 }
