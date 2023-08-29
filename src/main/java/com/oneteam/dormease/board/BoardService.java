ac44a71bcced397895693dc482fdec6d3a787770
ahnjisoo <wltn24996@gmail.com>
2023-08-30 08:59:23 +0900

    OB-33-자유-게시판-게시글-등록기능-추가

--- src/main/java/com/oneteam/dormease/board/BoardService.java
+++ src/main/java/com/oneteam/dormease/board/BoardService.java
@@ -2,8 +2,13 @@ package com.oneteam.dormease.board;
 
 import com.oneteam.dormease.user.student.StudentDto;
 import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Log4j2
 @Service
 public class BoardService {
@@ -13,6 +18,13 @@ public class BoardService {
     public int writeContentConfirm(StudentDto loginedStudentDto, BoardDto boardDto) {
         log.info("writeContentConfirm()");
 
-        return boardMapper.insertNewContent(loginedStudentDto, boardDto);
+        log.info("loginedStudentDto==>" + loginedStudentDto);
+        log.info("boardDto===========>" + boardDto);
+
+        Map<String, Object> boardDtoMap = new HashMap<>();
+        boardDtoMap.put("studentDto", loginedStudentDto);
+        boardDtoMap.put("boardDto", boardDto);
+
+        return boardMapper.insertNewContent(boardDtoMap);
     }
 }
