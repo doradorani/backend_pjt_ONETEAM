8d23a899c7babccbcffbe6ffd379e28839c2a538
ahnjisoo <wltn24996@gmail.com>
2023-08-30 19:20:24 +0900

    OB-36-자유-게시판-게시글-리스트보기-추가

--- src/main/java/com/oneteam/dormease/board/IBoardMapper.java
+++ src/main/java/com/oneteam/dormease/board/IBoardMapper.java
@@ -4,10 +4,13 @@ import com.oneteam.dormease.user.student.StudentDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface IBoardMapper {
 
     int insertNewContent(Map<String, Object> boardDtoMap);
+
+    List<BoardDto> selectAllFreeBoardContent(int schoolNo);
 }
