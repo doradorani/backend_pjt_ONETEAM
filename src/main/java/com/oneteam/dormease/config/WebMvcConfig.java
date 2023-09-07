ac82d74da39249219925d086509c14d4890b9104
jangdebug <jangdebug@gmail.com>
2023-09-07 12:54:44 +0900

    OB-88-학생-멤버링과-매점-및-인터셉트-연동

--- src/main/java/com/oneteam/dormease/config/WebMvcConfig.java
+++ src/main/java/com/oneteam/dormease/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.oneteam.dormease.config;
 
+import com.oneteam.dormease.user.parents.ProductInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +15,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/dormEaseUploadImg/**")
                 .addResourceLocations("file:///c://dormEase/upload/");
 
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new ProductInterceptor())
+                .addPathPatterns(
+                        "/product/**"
+                );
+
+        //유저 버전 멤버링 관련 파일은 이 밑으로  (임시로 만들어 놓음)
+        //registry.addInterceptor(new StudentInterceptor())
+        //                .addPathPatterns(
+        //                        "/student/**"
+        //                );
     }
 
 }
