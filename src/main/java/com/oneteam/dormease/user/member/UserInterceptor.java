00fff37dc3c8e74fd7ef972bb4c44186633135ea
doradorani <sokoyo2@gmail.com>
2023-09-07 22:03:50 +0900

    OB-97-학생-학부모-로그인-인터셉터

--- src/main/java/com/oneteam/dormease/user/member/UserInterceptor.java
+++ src/main/java/com/oneteam/dormease/user/member/UserInterceptor.java
@@ -1,24 +1,24 @@
-package com.oneteam.dormease.user.parents;
+package com.oneteam.dormease.user.member;
 
+import com.oneteam.dormease.user.parents.ParentsDto;
 import com.oneteam.dormease.user.student.StudentDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.util.HtmlUtils;
 
 import java.io.PrintWriter;
 
-public class ProductInterceptor implements HandlerInterceptor {
+public class UserInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
         HttpSession session = request.getSession();
+        ParentsDto loginedParentsDto = (ParentsDto) session.getAttribute("loginedParentsDto");
         StudentDto loginedStudentDto = (StudentDto) session.getAttribute("loginedStudentDto");
 
-        if(loginedStudentDto == null) {
+        if (loginedParentsDto == null && loginedStudentDto == null) {
 
-            String message = "alert('학교 학생만 사용이 가능합니다.');";
+            String message = "alert('로그인한 사용자만 사용이 가능합니다. 로그인을 해주세요!');";
 
             // 응답에 JavaScript 코드를 출력하고 문자 인코딩 설정
             response.setContentType("text/html; charset=UTF-8");
@@ -28,10 +28,11 @@ public class ProductInterceptor implements HandlerInterceptor {
 
             // JavaScript가 실행된 이후에 일정 시간(0.1초 후)에 리디렉션을 수행
             out.println("<script>setTimeout(function() { location.href = '" + request.getContextPath() + "/'; }, 100);</script>");
-
+            response.sendRedirect("/user/loginForm");
             return false;
         } else {
             return true;
         }
     }
+
 }
